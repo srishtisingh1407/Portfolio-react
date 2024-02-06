6d39fe5f93cabf3586d8b3dfd03ffee3bd9735cf
Srishtisingh1407 <srishtisingh140704@gmail.com>
2024-02-06 23:04:23 +0530

    add files

---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,22 @@
+
+
+export default function HeroSection( ){
+  return(
+    <section id="HeroSection" className="HeroSection">
+        <div className="HeroSection-content-box">
+            <div className="HeroSection-content">
+                <p className="section-title">
+                    Hey, I am srishti
+                </p>
+                <h1 className="HeroSection-title">
+                    <span className="HeroSection-title">
+                        Full Snack
+                    </span> {""}
+                    
+                </h1>
+            </div>
+        </div>
+
+    </section>
+  )
+}
